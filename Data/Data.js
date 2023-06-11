794eed1f9948de8bc3752f83313be036cadbcaef
NTienWork <nguyenngoctien.chatgpt@gmail.com>
2023-06-11 13:31:43 +0700

    Them Giao Dien Trang Home

--- Data/Data.js
+++ Data/Data.js
@@ -1,20 +1,77 @@
-const ItemCoffe = [
+export const TitleCoffe = [
     {
-        id : '1',
-        Titel : 'Americano'
+        id: '1',
+        Titel: 'Americano',
     },
     {
-        id : '2',
-        Titel : 'Latte'
+        id: '2',
+        Titel: 'Latte',
     },
     {
-        id : '3',
-        Titel : 'Chocolate'
+        id: '3',
+        Titel: 'Chocolate',
     },
     {
-        id : '4',
-        Titel : 'Coffe'
+        id: '4',
+        Titel: 'Coffee',
     },
-]
+    {
+        id: '5',
+        Titel: 'Capuchino',
+    },
+    {
+        id: '6',
+        Titel: 'Milk Coffee',
+    },
+];
+export const coffeeItems = [
+    {
+        id: 1,
+        name: 'Black Coffee',
+        price: '25.50',
+        volume: '116 ml',
+        stars: '4.6',
+        image: require('../assets/coffee2.png'),
+        desc: 'The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.',
+    },
 
-export default ItemCoffe
+    {
+        id: 2,
+        name: 'Cappuccino',
+        price: '15.50',
+        volume: '110 ml',
+        stars: '4.3',
+        image: require('../assets/coffee2.png'),
+        desc: 'The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.',
+    },
+
+    {
+        id: 3,
+        name: 'Espresso',
+        price: '30.00',
+        volume: '100 ml',
+        stars: '4.0',
+        image: require('../assets/coffee3.png'),
+        desc: 'The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.',
+    },
+
+    {
+        id: 4,
+        name: 'Latte',
+        price: '10.30',
+        volume: '80 ml',
+        stars: '3.5',
+        image: require('../assets/coffee4.png'),
+        desc: 'The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.',
+    },
+
+    {
+        id: 5,
+        name: 'Mocha',
+        price: '23.10',
+        volume: '120 ml',
+        stars: '4.7',
+        image: require('../assets/coffee5.png'),
+        desc: 'The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.',
+    },
+];
--- screen/Home/Home.tsx
+++ screen/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     View,
     Text,
@@ -10,11 +10,13 @@ import {
     TouchableOpacity,
     FlatList,
 } from 'react-native';
-import Coffe from '../../Data/Data.js';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
-import ItemCoffe from '../../Data/Data.js';
+import {TitleCoffe, coffeeItems} from '../../Data/Data';
+import Carousel from 'react-native-snap-carousel';
 
 export default function Home() {
+    const [ActiveBtn, setActiveBtn] = useState(1);
     return (
         <View style={{flex: 1, backgroundColor: 'white'}}>
             <StatusBar />
@@ -76,20 +78,179 @@ export default function Home() {
                         </TouchableOpacity>
                     </View>
                 </View>
-                <View>
+                <View style={styles.List_Tilte}>
                     <FlatList
                         horizontal
                         showsHorizontalScrollIndicator={false}
-                        data={Coffe}
+                        data={TitleCoffe}
                         keyExtractor={item => item.id}
-                        style={styles.List_Tilte}
                         renderItem={({item}) => {
+                            let ActiveColor = Number(item.id) == ActiveBtn;
                             return (
-                                <TouchableOpacity>
-                                    <Text>{item.Titel}</Text>
+                                <TouchableOpacity
+                                    onPress={() =>
+                                        setActiveBtn(Number(item.id))
+                                    }
+                                    style={{
+                                        padding: 18,
+                                        marginLeft: 5,
+                                        backgroundColor: ActiveColor
+                                            ? '#C29363'
+                                            : '#E2E0E2',
+                                        borderRadius: 50,
+                                    }}>
+                                    <Text
+                                        style={{
+                                            color: ActiveColor
+                                                ? 'white'
+                                                : 'black',
+                                            fontWeight: 'bold',
+                                        }}>
+                                        {item.Titel}
+                                    </Text>
                                 </TouchableOpacity>
                             );
                         }}></FlatList>
+                </View>
+                {/* Card  */}
+                <View style={{marginTop: 64, paddingBottom: 8, paddingTop: 8}}>
+                    <Carousel
+                        containerCustomStyle={{overflow: 'visible'}}
+                        data={coffeeItems}
+                        renderItem={({item}) => {
+                            return (
+                                <View
+                                    style={{
+                                        borderRadius: 40,
+                                        height: 350,
+                                        width: 250,
+                                        backgroundColor: '#83491A',
+                                    }}>
+                                    <View
+                                        style={[
+                                            styles.Card,
+                                            {
+                                                shadowColor: 'black',
+                                                shadowRadius: 30,
+                                                shadowOffset: {
+                                                    width: 0,
+                                                    height: 40,
+                                                },
+                                                shadowOpacity: 0.8,
+                                            },
+                                        ]}>
+                                        <Image
+                                            style={{width: 150, height: 150}}
+                                            source={item.image}
+                                        />
+                                    </View>
+                                    <View
+                                        style={{
+                                            paddingHorizontal: 20,
+                                            marginTop: 20,
+                                        }}>
+                                        <Text
+                                            style={{
+                                                fontSize: 30,
+                                                lineHeight: 36,
+                                                color: 'white',
+                                                zIndex: 10,
+                                            }}>
+                                            {item.name}
+                                        </Text>
+                                        <View
+                                            style={{
+                                                backgroundColor:
+                                                    'rgba(255,255,255,0.2)',
+                                                flexDirection: 'row',
+                                                alignItems: 'center',
+                                                borderRadius: 36,
+                                                padding: 4,
+                                                paddingHorizontal: 8,
+                                                width: 64,
+                                            }}>
+                                            <Text>
+                                                <Icon
+                                                    name="star"
+                                                    size={15}
+                                                    color="white"
+                                                />
+                                            </Text>
+                                            <Text
+                                                style={{
+                                                    fontSize: 16,
+                                                    lineHeight: 24,
+                                                    color: 'white',
+                                                    fontWeight: 'bold',
+                                                }}>
+                                                {item.stars}
+                                            </Text>
+                                        </View>
+                                        <View
+                                            style={{
+                                                flexDirection: 'row',
+                                                alignItems: 'center',
+                                                marginTop: 10,
+                                            }}>
+                                            <Text
+                                                style={{
+                                                    marginLeft: 5,
+                                                    fontSize: 18,
+                                                    color: 'white',
+                                                }}>
+                                                Size :
+                                            </Text>
+                                            <TouchableOpacity
+                                                style={styles.Btn_Size_Card}>
+                                                <Text>S</Text>
+                                            </TouchableOpacity>
+                                            <TouchableOpacity
+                                                style={styles.Btn_Size_Card}>
+                                                <Text>M</Text>
+                                            </TouchableOpacity>
+                                        </View>
+                                        <View
+                                            style={{
+                                                flexDirection: 'row',
+                                                justifyContent: 'space-between',
+                                                alignItems: 'center',
+                                                marginTop: 15,
+                                            }}>
+                                            <Text
+                                                style={{
+                                                    fontSize: 18,
+                                                    color: 'white',
+                                                }}>
+                                                $ {item.price}
+                                            </Text>
+                                            <TouchableOpacity
+                                                style={{
+                                                    backgroundColor: 'white',
+                                                    padding: 10,
+                                                    alignItems: 'center',
+                                                    justifyContent: 'center',
+                                                    borderRadius: 999,
+                                                }}>
+                                                <Text>
+                                                    <Icon
+                                                        name="plus"
+                                                        size={25}
+                                                        color="#83491A"
+                                                    />
+                                                </Text>
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View>
+                                </View>
+                            );
+                        }}
+                        firstItem={1}
+                        inactiveSlideOpacity={0.5}
+                        inactiveSlideScale={0.77}
+                        sliderWidth={400}
+                        itemWidth={260}
+                        slideStyle={{display: 'flex', alignItems: 'center'}}
+                    />
                 </View>
             </SafeAreaView>
         </View>
@@ -115,5 +276,20 @@ const styles = StyleSheet.create({
     },
     List_Tilte: {
         marginTop: 30,
+        paddingLeft: 15,
+        marginRight: 2,
+    },
+    Card: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    Btn_Size_Card: {
+        backgroundColor: '#fff',
+        padding: 7,
+        width: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 50,
+        marginLeft: 5,
     },
 });
